4ba0b32192a10f013123ad3548c133d873d9e231
b166erobot <bernard0freitas@outlook.com>
2024-02-06 18:12:16 -0300

    Requisito 2 satisfeito

---
+++ script.js
@@ -0,0 +1,11 @@
+// obtendo elementos.
+const input = document.querySelector('#carta-texto');
+const pResult = document.querySelector('#carta-gerada');
+const buttonCreateLetter = document.querySelector('#criar-carta');
+
+// configurando elementos.
+buttonCreateLetter.innerText = 'criar carta';
+input.classList.add('inline-block-widgets');
+buttonCreateLetter.classList.add('inline-block-widgets');
+
+// código.
